2525f29838c35e5fffdde89afad62d34d4577942
brunaletsleticia <bruna777leticia@gmail.com>
2025-09-27 23:27:35 -0300

    resolvendo conflritos

--- app/common/contexts/index.ts
+++ app/common/contexts/index.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 export * from "./app";
 export * from "./client";
 export * from "./admin";
-=======
-export * from "./admin";
-export * from "./app";
-export * from "./client";
->>>>>>> 82c00265b44286a4cf7bd413917921aa91196f00
